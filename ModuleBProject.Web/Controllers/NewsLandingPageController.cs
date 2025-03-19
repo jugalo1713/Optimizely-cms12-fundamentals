5102bb7cbf020718e4fd52f89fa812e4c2c11124
JulianGalloNexer <julian.gallo2@nexergroup.com>
2025-03-19 16:04:53 -0500

    E2 finished

---
+++ ModuleBProject.Web/Controllers/NewsLandingPageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModuleBProject.Web.Models.Pages;
+
+namespace ModuleBProject.Web.Controllers
+{
+    public class NewsLandingPageController : PageControllerBase<NewsLandingPage>
+    {
+        public NewsLandingPageController(IContentLoader loader) : base(loader)
+        {
+        }
+        public IActionResult Index(NewsLandingPage currentPage)
+        {
+            return View(CreatePageViewModel(currentPage));
+        }
+    }
+}
