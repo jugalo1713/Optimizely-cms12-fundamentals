092ceb0706526df4bf9dc5b09a2257a2cce3f784
JulianGalloNexer <julian.gallo2@nexergroup.com>
2025-03-19 12:06:07 -0500

    D1 finished

---
+++ ModuleBProject.Web/Models/Blocks/EditorialBlock.cs
@@ -0,0 +1,25 @@
+﻿using ModuleBProject.Web.constants;
+using ModuleBProject.Web.Icons;
+using ModuleBProject.Web.Models.Pages;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModuleBProject.Web.Models.Blocks
+{
+    [ContentType(
+        DisplayName = "Editorial",
+        GUID = "{AC35F72B-0418-4F1F-9F4D-DDA5580BD250}",
+        Description = "UUse this for a rich editorial text that will be reused in multiple places",
+        GroupName = SiteGroupNames.Common
+        )]
+    [SiteBlockIcon]
+    public class EditorialBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main Body",
+            GroupName = SystemTabNames.Content,
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables",
+            Order = 10)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
